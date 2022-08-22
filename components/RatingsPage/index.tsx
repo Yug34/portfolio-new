8933a0d4627b32e243e5591b90af248e778409b5
Yug34 <vertilax1337@gmail.com>
2022-08-22 17:37:57 +0530

    :sparkles: feat(project): added review form

--- components/RatingsPage/index.tsx
+++ components/RatingsPage/index.tsx
@@ -1,12 +1,15 @@
 import { HeartSVG } from "./HeartSVG";
 import { capitalizeFirstLetter } from "../../utils";
 import * as Styles from "./RatingsPage.Styles";
-// import { useMutation } from "@apollo/client";
+import {useMutation} from "@apollo/client";
+import {INSERT_RATINGS_ONE} from "../../graphql/queries";
+import { useForm } from 'react-hook-form';
 
 interface RatingsPagePropType {
     ratings: {
         id: number;
-        reviewer: string;
+        reviewer_first_name: string;
+        reviewer_last_name?: string | null;
         category: number;
         comment: string;
         rating: number;
@@ -16,6 +19,18 @@ interface RatingsPagePropType {
 
 const RatingsPage = (props: RatingsPagePropType) => {
     const { ratings } = props;
+
+    const onSubmit = (data: any) => {
+        console.log(data)
+    }
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
+
+    const [insertRating, { data, loading, error }] = useMutation(INSERT_RATINGS_ONE)
+
     const getReadableDate = (date: Date) => date.toDateString().split(' ').slice(1).join(' ');
 
     return (
@@ -27,7 +42,7 @@ const RatingsPage = (props: RatingsPagePropType) => {
                             <HeartSVG count={rating.rating} />
                             <Styles.CommentText>{capitalizeFirstLetter(rating.comment)}</Styles.CommentText>
                             <Styles.AuthorDateContainer>
-                                <Styles.Author>- {capitalizeFirstLetter(rating.reviewer)}</Styles.Author>
+                                <Styles.Author>- {capitalizeFirstLetter(rating.reviewer_first_name)} {capitalizeFirstLetter(rating.reviewer_last_name ? rating.reviewer_last_name : "")}</Styles.Author>
                                 <Styles.DateContainer>{getReadableDate(new Date(rating.created_at))}</Styles.DateContainer>
                             </Styles.AuthorDateContainer>
                         </Styles.Rating>
@@ -37,7 +52,36 @@ const RatingsPage = (props: RatingsPagePropType) => {
                     </div>
                 )
             })}
-            {/*<div onClick={() => {console.log("Hey")}}>submit</div>*/}
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="email" placeholder="Email" {...register('email', { required: true })} />
+                {errors.email && <span>This is a required field</span>}
+                <input type="text" placeholder="First Name" {...register('firstName', { required: true, min: 5, max: 20 })} />
+                {errors.firstName && <span>This is a required field</span>}
+                <input type="text" placeholder="Last Name (Optional)" {...register('lastName', { max: 20 })} />
+                <input type="number" placeholder={`Rating: 1.0 to 5.0`} {...register('rating', { required: true, min: 1, max: 5 })} />
+                {errors.rating && <span>This is a required field</span>}
+                <input type="text" placeholder="comment" {...register('comment', {required: true, minLength: 10, maxLength: 300 })} />
+                {errors.comment && <span>This is a required field</span>}
+                <input type="submit"/>
+            </form>
+
+            <div onClick={(e) => {
+                e.preventDefault();
+                insertRating({
+                    variables: {
+                        reviewer_first_name: "random",
+                        reviewer_last_name: "idk",
+                        category: 2,
+                        comment: "insert success",
+                        rating: 4.52,
+                        email: "testMail"
+                    }
+                })
+                }}
+            >
+                Submit
+            </div>
         </Styles.RatingsContainer>
     );
 };
--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,17 +1,16 @@
 import {gql} from "@apollo/client";
 
-// export const INSERT_RATING = gql`mutation {
-//     insert_ratings(objects: {category: 2, comment: "idk", rating: 2, reviewer: "me"}) {
-//         returning {
-//           id
-//         }
-//     }
-// }`;
+export const INSERT_RATINGS_ONE = gql`mutation($email: String!, $reviewer_first_name: String!, $reviewer_last_name: String!, $category: Int!, $comment: String!, $rating: numeric!) {
+    insert_ratings_one(object: {email: $email, reviewer_first_name: $reviewer_first_name, reviewer_last_name: $reviewer_last_name, category: $category, comment: $comment, rating: $rating}) {
+        id
+    }
+}`;
 
 export const GET_RATINGS = gql`query getRatings {
     ratings {
         id
-        reviewer
+        reviewer_first_name
+        reviewer_last_name
         category
         comment
         rating
