e43d5f3d55c9e280b0c41f42185fdc1d2ed94d0e
Yug34 <vertilax1337@gmail.com>
2022-10-26 16:24:33 +0530

    :sparkles: feat(contact): added contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,8 +1,46 @@
 import type {NextPage} from 'next'
+import { Hover } from '../components/Common/Hover';
+import { Flex } from '../components/Common/Flex';
 
 const Contact: NextPage = () => {
     return (
-        <div>Hey</div>
+        <Flex flexDirection='column' justify='center' align='center' rowGap='6px'>
+            <div>Will integrade a contact form soon, almost done with grad school applications!</div>
+            <div>
+                Email:
+                <Hover
+                    style={{marginLeft: '4px'}}
+                    monoState
+                    externalHref={true}
+                    fontWeight={600}
+                    href={"mailto:yug.gjr@gmail.com"}
+                >
+                    yug.gjr@gmail.com
+                </Hover>
+            </div>
+
+            <Flex colGap='6px' justify='center' align='center'>
+                <Hover
+                    monoState
+                    externalHref={true}
+                    fontWeight={600}
+                    href={"https://www.linkedin.com/in/yug34/"}
+                >
+                    LinkedIn
+                </Hover>
+
+                |
+
+                <Hover
+                    monoState
+                    externalHref={true}
+                    fontWeight={600}
+                    href={"https://github.com/Yug34"}
+                >
+                    GitHub
+                </Hover>
+            </Flex>
+        </Flex>
     );
 };
 
